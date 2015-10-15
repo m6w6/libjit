68338121953d5dd482824e0ed0e9f9368f1929f1
Michael Wallner <mike@php.net>
2015-10-15 20:21:14 +0200

    fix pointer truncation due to missing cast to jit_nuint

--- jit/jit-memory-cache.c
+++ jit/jit-memory-cache.c
@@ -643,7 +643,7 @@ _jit_cache_alloc_data(jit_cache_t cache, unsigned long size, unsigned long align
 	   overlap with the function code possibly being written at the bottom
 	   of the free region */
 	ptr = cache->free_end - size;
-	ptr = (unsigned char *) (((jit_nuint) ptr) & ~(align - 1));
+	ptr = (unsigned char *) (((jit_nuint) ptr) & ~((jit_nuint) align - 1));
 	if(ptr < cache->free_start)
 	{
 		/* When we aligned the block, it caused an overflow */
@@ -675,7 +675,7 @@ alloc_code(jit_cache_t cache, unsigned int size, unsigned int align)
 	ptr = cache->free_start;
 	if(align > 1)
 	{
-		jit_nuint p = ((jit_nuint) ptr + align - 1) & ~(align - 1);
+		jit_nuint p = ((jit_nuint) ptr + align - 1) & ~((jit_nuint) align - 1);
 		ptr = (unsigned char *) p;
 	}
 
@@ -695,7 +695,7 @@ alloc_code(jit_cache_t cache, unsigned int size, unsigned int align)
 		ptr = cache->free_start;
 		if(align > 1)
 		{
-			jit_nuint p = ((jit_nuint) ptr + align - 1) & ~(align - 1);
+			jit_nuint p = ((jit_nuint) ptr + align - 1) & ~((jit_nuint) align - 1);
 			ptr = (unsigned char *) p;
 		}
 	}
